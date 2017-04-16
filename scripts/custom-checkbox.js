0ea42bbb3cc9dc2782e596f4d3d531e96cef5c8b
bartosz.sroka <bartosz.sroka@compactbi.com>
2017-04-16 21:45:36 +0200

    Move scripts the end of the body section

--- scripts/custom-checkbox.js
+++ scripts/custom-checkbox.js
@@ -1,39 +1,31 @@
 'use strict';
 
 (function () {
-    var checkBoxToggle = function () {
-        var checkedAttributeName = "aria-checked";
+    var checkedAttributeName = "aria-checked";
 
-        var toggleCheckBox = function (elem) {
-            if (elem.getAttribute(checkedAttributeName) === "true") {
-                elem.setAttribute(checkedAttributeName, "false");
-            } else {
-                elem.setAttribute(checkedAttributeName, "true");
-            }
-        };
-
-        var checkBoxLabels = document.querySelectorAll(".b-checkbox-container__label");
-
-        var toggleCheckBoxFunction = function (e) {
-            toggleCheckBox(e.srcElement);
-        };
-
-        var clickCheckBoxFunction = function (e) {
-            e.preventDefault();
-            if (e.which === 32 || e.which === 13) {
-                e.srcElement.click();
-            }
-        };
-
-        Object.keys(checkBoxLabels).forEach(function (index) {
-            checkBoxLabels[index].addEventListener("click", toggleCheckBoxFunction);
-            checkBoxLabels[index].addEventListener("keypress", clickCheckBoxFunction);
-        });
+    var toggleCheckBox = function (elem) {
+        if (elem.getAttribute(checkedAttributeName) === "true") {
+            elem.setAttribute(checkedAttributeName, "false");
+        } else {
+            elem.setAttribute(checkedAttributeName, "true");
+        }
     };
 
-    if (document.readyState !== "loading") {
-        checkBoxToggle();
-    } else {
-        document.addEventListener("DOMContentLoaded", checkBoxToggle);
-    }
+    var checkBoxLabels = document.querySelectorAll(".b-checkbox-container__label");
+
+    var toggleCheckBoxFunction = function (e) {
+        toggleCheckBox(e.srcElement);
+    };
+
+    var clickCheckBoxFunction = function (e) {
+        e.preventDefault();
+        if (e.which === 32 || e.which === 13) {
+            e.srcElement.click();
+        }
+    };
+
+    Object.keys(checkBoxLabels).forEach(function (index) {
+        checkBoxLabels[index].addEventListener("click", toggleCheckBoxFunction);
+        checkBoxLabels[index].addEventListener("keypress", clickCheckBoxFunction);
+    });
 }());
